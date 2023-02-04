4903b5e4d6d4c261b11c1607e6810210c645c41f
Marcio Dias <98918078+MarcioBADias@users.noreply.github.com>
2023-02-04 17:22:21 -0300

    Global Styles

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,8 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
 const Global = createGlobalStyle`
+
+    @import url('https://fonts.googleapis.com/css2?family=Ruda:wght@400;500;700&display=swap');
+
     *{
         box-sizing: border-box;
+        font-family: 'Ruda', sans-serif;
         padding: 0;
         margin: 0;
     }
@@ -10,6 +14,17 @@ const Global = createGlobalStyle`
     body {
         background-color: #000;
     }
+
+    a{
+        text-decoration: none;
+        color: #f7d354;
+        transition: 0.5s;
+
+        :hover{
+            color: #b8930c
+        }
+    }
+    
 
 `;
 
